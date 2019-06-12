37eaf1f30adc875449b03162d0891e64663265a1
Ashdark <48515854+Ashdark@users.noreply.github.com>
2019-06-12 11:19:15 -0300

    Add files via upload

--- AtividadeFinalPontoDigital/Controllers/LoginController.cs
+++ AtividadeFinalPontoDigital/Controllers/LoginController.cs
@@ -1,9 +1,43 @@
+using AtividadeFinalPontoDigital.Models;
+using AtividadeFinalPontoDigital.Repositorio;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AtividadeFinalPontoDigital.Controllers
 {
     public class LoginController : Controller
     {
-        
+        [HttpPost]
+        public IActionResult Logar (IFormCollection form) {
+            UsuarioRepositorio usuarioRepositorio = new UsuarioRepositorio ();
+            if(usuarioRepositorio.BuscarUsuario (form["email"], form["senha"])){
+                return RedirectToAction("Logged");
+            } else if(usuarioRepositorio.BuscarAdministrador(form["email"], form["senha"])){
+                return RedirectToAction("LoggedAdmin");
+            } else{
+            return RedirectToAction("Login","Home");
+            }
+
     }
+    public IActionResult Logged(){
+        ViewData["NomeView"] = "Logado!";
+        return View();
+    }
+        public IActionResult Comentarios(){
+        ViewData["NomeView"] = "Comentarios - Ponto Digital";
+        return View();
+    }
+        public IActionResult Sobre(){
+        ViewData["NomeView"] = "Sobre - Ponto Digital";
+        return View();
+    }
+        public IActionResult Index(){
+        ViewData["NomeView"] = "Home - Ponto Digital";
+        return View();
+    }
+            public IActionResult LoggedAdmin(){
+            ViewData["NomeView"] = "Administrador na área, hora do ban";
+            return View();
+        }
+}
 }
